f18a5d3516f26bf174c6ef9de0c2109df79c18e6
Diksha <dikshabuxani84745@gmail.com>
2023-02-25 16:58:11 +0530

    add - server memebership file

---
+++ server/routes/membershipRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const MemberDetails = require('../models/MemShipModel')
+const CryptoUser = require("../models/User")
+
+
+router.post('/addmember', async (req,res)=>{
+     
+    const { memberShip , date , noOfinvested , email } = req.body;
+
+    try {
+        const existinguser = await CryptoUser.findOne({ emailAddress: email })
+        
+        const membership = await new MemberDetails({
+            userEmail: email,
+            memberShip: memberShip,
+            startDateMem: date,
+            noOfinvested: noOfinvested
+        }).save();
+        res.status(200).json({
+            success: true,
+            data: membership
+        })
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+
+module.exports = router;
