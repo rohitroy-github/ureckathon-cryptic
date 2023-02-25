a2b0a072363939059bc1f3ad93482281768f4881
Diksha <dikshabuxani84745@gmail.com>
2023-02-25 22:26:27 +0530

    client - adding constants file

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+// constantsForBlockchainConnection
+import abi from "./Transactions.json";
+export const contractABI = abi.abi;
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
