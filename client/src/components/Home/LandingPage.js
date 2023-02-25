e4698da44ba5d626c67818493b78e141c34d2c9c
rohit <rhtry.tech@gmail.com>
2023-02-25 15:35:37 +0530

    add : Home page !

---
+++ client/src/components/Home/LandingPage.js
@@ -0,0 +1,34 @@
+import React, {useState, useRef} from "react";
+import HeaderComp from "./home-components/HeaderComp";
+import CoverComp from "./home-components/CoverComp";
+import FaqComp from "./home-components/FaqComp";
+import FooterComp from "./home-components/FooterComp";
+import {Login} from "./home-components/LoginComp";
+import {Register} from "./home-components/RegisterComp";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  const [currentForm, setCurrentForm] = useState("login");
+  const loginref = useRef();
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+  return (
+    <div className="app">
+      <HeaderComp onFormSwitch={toggleForm} scrollTo={loginref} />
+      <div className="Form">
+        {currentForm === "login" ? (
+          <Login onFormSwitch={toggleForm} />
+        ) : (
+          <Register onFormSwitch={toggleForm} />
+        )}
+      </div>
+      <CoverComp scrollTo={loginref} />
+      <FaqComp scrollTo={loginref} />
+      <FooterComp />
+    </div>
+  );
+};
+
+export default LandingPage;
