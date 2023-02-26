5eb9dad2ff25f9a0d0b3cc4b1ef706be06d83553
rohit <rhtry.tech@gmail.com>
2023-02-26 15:16:54 +0530

    add : components

---
+++ client/src/components/YourBid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import sxprop from './sxStyle';
+
+
+const YourBid = () => {
+  return (
+    <Box sx={sxprop.subboxsx}>Your Bid</Box>
+    
+  )
+}
+
+export default YourBid
